a3f902b12a6c4bd8da894e45a890019d84906576
FlorianManghofer <florian.manghofer@gmail.com>
2025-09-18 21:57:14 +0200

    Vuetify installiert

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// Vuetify in die App einhängen
+app.use(vuetify)
 
 app.mount('#app')
